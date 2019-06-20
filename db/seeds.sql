ea34705fab4fb686ae0b79a616f579ab51c6cd87
Sherrie McNulty <sherriemcnulty@gmail.com>
2019-06-19 20:41:00 -0400

    Got rid of delete.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Dagwood Stack');
 INSERT INTO burgers (name) VALUES ('Scooby Snack');
-INSERT INTO burgers (name, sleepy) VALUES ('Garfield Fatty', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Garfield Big-Boy', true);
 INSERT INTO burgers (name, sleepy) VALUES ('Kahlees', true);
 INSERT INTO burgers (name, sleepy) VALUES ('Wimpy Burger', true);
 INSERT INTO burgers (name) VALUES ('Spock Shock');
